132f49806f06a2b26e0f78f18b4ac8eec0e9f144
AXeL-dev <contact.axel.dev@gmail.com>
2021-06-19 00:48:10 +0100

    chore: update unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,29 +1,10 @@
-import { useMyHook } from './'
-import { renderHook, act } from "@testing-library/react-hooks";
+import useCancelToken from './';
+import { renderHook } from '@testing-library/react-hooks';
 
-// mock timer using jest
-jest.useFakeTimers();
+describe('useCancelToken', () => {
+  it('renders correctly', () => {
+    const { result } = renderHook(() => useCancelToken());
 
-describe('useMyHook', () => {
-  it('updates every second', () => {
-    const { result } = renderHook(() => useMyHook());
-
-    expect(result.current).toBe(0);
-
-    // Fast-forward 1sec
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
-
-    // Check after total 1 sec
-    expect(result.current).toBe(1);
-
-    // Fast-forward 1 more sec
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
-
-    // Check after total 2 sec
-    expect(result.current).toBe(2);
-  })
-})
+    expect(result.current).toMatchObject({});
+  });
+});
