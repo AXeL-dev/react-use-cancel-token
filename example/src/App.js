d30f776ec4f0326c83e2c38cadf8199e56b184c4
AXeL-dev <contact.axel.dev@gmail.com>
2021-06-19 00:48:22 +0100

    chore: add demo example

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,81 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import _ from 'lodash';
 
-import { useMyHook } from 'react-use-cancel-token'
+import useCancelToken from 'react-use-cancel-token';
 
 const App = () => {
-  const example = useMyHook()
+  const [search, setSearch] = React.useState('');
+  const [searching, setSearching] = React.useState(false);
+  const [result, setResult] = React.useState('No results.');
+  const { newCancelToken, cancelPreviousRequest, isCancel } = useCancelToken();
+
+  const performSearch = async (keywords = search) => {
+    cancelPreviousRequest();
+    setSearching(true);
+
+    try {
+      const response = await axios.get(
+        `https://api.github.com/search/repositories?q=${keywords}&sort=stars&order=desc`,
+        {
+          cancelToken: newCancelToken(),
+        }
+      );
+
+      if (response.status === 200) {
+        setResult(response.data.items);
+      }
+    } catch (err) {
+      if (isCancel(err)) return;
+      setResult('An error occured!');
+      console.error(err);
+    }
+
+    setSearching(false);
+  };
+
+  const delayedSearch = React.useCallback(
+    _.debounce((value) => performSearch(value), 700),
+    [performSearch]
+  );
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setSearch(value);
+    delayedSearch(value);
+  };
+
+  const handleCancel = () => {
+    cancelPreviousRequest();
+    setSearching(false);
+  };
+
   return (
-    <div>
-      {example}
-    </div>
-  )
-}
-export default App
+    <>
+      <input type="text" placeholder="Search..." value={search} onChange={handleInputChange} />
+      <button onClick={handleCancel} disabled={!searching}>
+        Cancel
+      </button>
+      {searching && ' Searching...'}
+      <br />
+      <br />
+      <div>
+        {_.isArray(result) ? (
+          <>
+            <h3>Github repositories:</h3>
+            <ul>
+              {result.map((item, index) => (
+                <li key={index}>
+                  <a href={item.html_url} target="_blank" rel="noopener noreferrer">{item.name}</a>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          result
+        )}
+      </div>
+    </>
+  );
+};
+export default App;
