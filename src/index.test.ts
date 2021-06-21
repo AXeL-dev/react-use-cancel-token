0769c7fc8f18c03d483773e27dd780c68a672c2e
AXeL-dev <contact.axel.dev@gmail.com>
2021-06-21 21:58:57 +0100

    chore: enhance unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,58 @@
 import useCancelToken from './';
 import { renderHook } from '@testing-library/react-hooks';
+jest.mock('./useCancelToken');
+
+const isCancelReturnValue = false;
+const useCancelTokenMock = {
+  newCancelToken: jest.fn(),
+  cancelPreviousRequest: jest.fn(),
+  isCancel: jest.fn().mockReturnValue(isCancelReturnValue),
+};
 
 describe('useCancelToken', () => {
+  beforeEach(() => {
+    (useCancelToken as jest.Mock).mockReturnValue(useCancelTokenMock);
+  });
+
   it('renders correctly', () => {
     const { result } = renderHook(() => useCancelToken());
 
-    expect(result.current).toMatchObject({});
+    expect(result.current).toEqual(
+      expect.objectContaining({
+        newCancelToken: expect.any(Function),
+        cancelPreviousRequest: expect.any(Function),
+        isCancel: expect.any(Function),
+      })
+    );
+  });
+
+  it('should return a new token', () => {
+    const { result } = renderHook(() => useCancelToken());
+    result.current.newCancelToken();
+
+    expect(useCancelTokenMock.newCancelToken).toHaveBeenCalledTimes(1);
+  });
+
+  it('should cancel the previous request on demand', () => {
+    const { result } = renderHook(() => useCancelToken());
+    result.current.cancelPreviousRequest();
+
+    expect(useCancelTokenMock.cancelPreviousRequest).toHaveBeenCalledTimes(1);
+  });
+
+  it('should cancel any on going request on unmount', () => {
+    const { unmount } = renderHook(() => useCancelToken());
+    unmount();
+
+    expect(useCancelTokenMock.cancelPreviousRequest).toHaveBeenCalledTimes(1);
+  });
+
+  it('should verify if the request has been canceled', () => {
+    const { result } = renderHook(() => useCancelToken());
+    const error = new Error('request canceled.');
+    result.current.isCancel(error);
+
+    expect(useCancelTokenMock.isCancel).toHaveBeenCalledTimes(1);
+    expect(useCancelTokenMock.isCancel).toHaveReturnedWith(isCancelReturnValue);
   });
 });
