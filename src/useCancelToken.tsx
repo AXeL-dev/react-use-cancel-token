c37130eaa86364675ec1f97ac0bb4920efbc62a6
AXeL-dev <contact.axel.dev@gmail.com>
2022-05-13 20:59:11 +0100

    chore: deprecate useCancelToken hook

--- src/useCancelToken.tsx
+++ src/useCancelToken.tsx
@@ -10,6 +10,7 @@ import { UseCancelToken, SourceRef, CancelToken } from './types';
  * This hook also allows manual requests cancellation.
  * https://dev.to/tmns/usecanceltoken-a-custom-react-hook-for-cancelling-axios-requests-1ia4
  *
+ * @deprecated since version 1.0.3. Use the useAbortController hook instead.
  * @returns {source: ref, newCancelToken: function, cancelPreviousRequest: function, isCancel: function}
  * source - used to access/set Axios cancel token source.
  * newCancelToken - used to generate the cancel token sent in the Axios request.
